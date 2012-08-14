66bf06c9828802146a79b481cd0ec142fac8b515
Vincent BESANCON <besancon.vincent@gmail.com>
2012-08-14 23:47:06 +0200

    Upgraded fabfiles to latest monitoring.fabric release.

--- fabdev.py
+++ fabdev.py
@@ -18,5 +18,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
 
-from monitoring.fabric.deb import release, bugfix
-
+from monitoring.fabric.deb import *
